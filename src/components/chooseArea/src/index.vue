fdabc2acba32d98fb34c9c2983b1d62af2d40587
lzs2018 <lzs2609534520@gmail.com>
2024-07-10 08:21:05 +0800

    [新增]：新增省市区数据传递父组件

--- src/components/chooseArea/src/index.vue
+++ src/components/chooseArea/src/index.vue
@@ -1,47 +1,123 @@
 <template>
-  <div style="display: flex;gap: 10px;">
+  <div style="display: flex; gap: 10px">
     <el-select clearable placeholder="请选择省份" v-model="province">
-      <el-option v-for="item in areas" :key="item.code" :value='item.code' :label="item.name"></el-option>
+      <el-option
+        v-for="item in areas"
+        :key="item.code"
+        :value="item.code"
+        :label="item.name"
+      ></el-option>
     </el-select>
-    <el-select clearable :disbaled="!province" placeholder="请选择城市" v-model="city">
-      <el-option v-for="item in selectCity" :key="item.code" :value='item.code' :label="item.name"></el-option>
+    <el-select
+      clearable
+      :disbaled="!province"
+      placeholder="请选择城市"
+      v-model="city"
+    >
+      <el-option
+        v-for="item in selectCity"
+        :key="item.code"
+        :value="item.code"
+        :label="item.name"
+      ></el-option>
     </el-select>
-    <el-select clearable :disbaled="!province || !city" placeholder="请选择区域" v-model="area">
-      <el-option v-for="item in selectArea" :key="item.code" :value='item.code' :label="item.name"></el-option>
+    <el-select
+      clearable
+      :disbaled="!province || !city"
+      placeholder="请选择区域"
+      v-model="area"
+    >
+      <el-option
+        v-for="item in selectArea"
+        :key="item.code"
+        :value="item.code"
+        :label="item.name"
+      ></el-option>
     </el-select>
   </div>
 </template>
 
-<script lang='ts' setup>
-import { ref, reactive, computed, watch, onMounted } from 'vue';
-import allAreas from './lib/pca-code.json'
-const province = ref<string>('')
-const city = ref<string>('')
-const area = ref<string>('')
-console.log(allAreas)
-let areas = ref(allAreas)
+<script lang="ts" setup>
+import { ref, reactive, computed, watch, onMounted } from "vue";
+import allAreas from "./lib/pca-code.json";
+const province = ref<string>("");
+const city = ref<string>("");
+const area = ref<string>("");
+console.log(allAreas);
+let areas = ref(allAreas);
 
-let selectCity = ref<any>([])
+export interface AreaItem {
+  name: string;
+  code: string;
+  children?: AreaItem[];
+}
 
-let selectArea = ref<any>([])
+export interface Data {
+  name: string;
+  code: string;
+}
 
-watch(()=> province.value,(val)=> {
-  if(val){
-    let cities = areas.value.find(item => item.code === province.value)!.children
-    selectCity.value =  cities
+let selectCity = ref<AreaItem[]>([]);
+
+let selectArea = ref<AreaItem[]>([]);
+
+let emits = defineEmits(["change"]);
+
+watch(
+  () => province.value,
+  (val) => {
+    if (val) {
+      let cities = areas.value.find(
+        (item) => item.code === province.value
+      )!.children;
+      selectCity.value = cities;
+    }
+    city.value = "";
+    area.value = "";
   }
-  city.value = ''
-  area.value = ''
-})
+);
 
-watch(()=> city.value,(val)=> {
-  if(val){
-    let area = selectCity.value.find(item => item.code === city.value)!.children
-    selectArea.value =  area
+watch(
+  () => city.value,
+  (val) => {
+    if (val) {
+      let area = selectCity.value.find((item) => item.code === city.value)!
+        .children!;
+      selectArea.value = area;
+    }
+    area.value = "";
   }
-  area.value = ''
-})
+);
+
+//
+watch(
+  () => area.value,
+  (val) => {
+    if (val) {
+      let provinceData: Data = {
+        code: province.value,
+        name:
+          province.value &&
+          allAreas.find((item) => item.code === province.value)!.name,
+      };
+      let cityData: Data = {
+        code: city.value,
+        name:
+          city.value &&
+          selectCity.value.find((item) => item.code === city.value)!.name,
+      };
+      let areaData: Data = {
+        code: val,
+        name: val && selectArea.value.find((item) => item.code === val)!.name,
+      };
+      emits("change", {
+        province: provinceData,
+        city: cityData,
+        area: areaData,
+      });
+    }
+  }
+);
 </script>
 
-<style lang='scss' scoped>
-</style>
+<style lang="scss" scoped></style>
--- src/views/chooseArea/index.vue
+++ src/views/chooseArea/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <m-choose-area />
+    <m-choose-area @change="changeArea" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive, computed, watch, onMounted } from "vue";
+const changeArea = (val: any) => {
+  console.log(val);
+};
 </script>
 
 <style lang="less" scoped></style>
